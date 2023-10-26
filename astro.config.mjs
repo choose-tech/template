5d2def4e8a2b303501739b00c90ec8bce3e8fe2b
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-10-26 15:39:01 +0200

    feat: upgrade integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from "astro/config";
 import chooseTechIntegration from "@choose-tech/astro";
-import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  // TODO: move to integration
-  srcDir: ".",
   integrations: [
-    tailwind(),
     chooseTechIntegration({
       base: "/template",
       title: "Template",
